1e4936c688bffd0f494fa74c11c6bfc3e913cdf2
kcshubarna <kcsh@sheridancollege.ca>
2024-05-15 12:23:50 -0400

    Revert "first git project"
    This reverts commit 6a9684f518d3f323147abcc824c2be1698920703.

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -16,7 +16,6 @@ public class StudentList {
     public static void main(String[] args) {
         
         System.out.println("hello world!");
-        System.out.println("Another hello world!");
         // TODO code application logic here
     }
     
