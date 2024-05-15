6a9684f518d3f323147abcc824c2be1698920703
kcshubarna <kcsh@sheridancollege.ca>
2024-05-15 12:18:31 -0400

    first git project

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -16,6 +16,7 @@ public class StudentList {
     public static void main(String[] args) {
         
         System.out.println("hello world!");
+        System.out.println("Another hello world!");
         // TODO code application logic here
     }
     
