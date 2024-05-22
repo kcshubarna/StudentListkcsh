1d774200efcee7c5bb9c86a98f7db5fdafaa300c
kcshubarna <kcsh@sheridancollege.ca>
2024-05-22 11:46:16 -0400

    first git project

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -9,5 +9,32 @@ package studentlist;
  * @author Shusil Kc
  */
 public class Student {
-    
+    private String name;
+    private String address;
+    private String studentId; 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
 }
--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -10,13 +10,5 @@ package studentlist;
  */
 public class StudentList {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        System.out.println("hello world!");
-        // TODO code application logic here
-    }
-    
+   
 }
